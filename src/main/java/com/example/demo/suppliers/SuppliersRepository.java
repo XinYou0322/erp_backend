1155328f83074a9da1d6701326cded150769ee5d
kellymo0118 <kelly230547@gmail.com>
2026-08-30 22:20:28 +0800

    Entity : PurchaseOrdersItems , 其餘mapped

---
+++ src/main/java/com/example/demo/suppliers/SuppliersRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.suppliers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SuppliersRepository extends JpaRepository<Suppliers, Long> {
+    
+}
---
+++ src/main/java/com/example/demo/suppliers/SuppliersService.java
@@ -0,0 +1,15 @@
+package com.example.demo.suppliers;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SuppliersService {
+    
+    private final SuppliersRepository suppliersRepo;
+
+
+
+}
