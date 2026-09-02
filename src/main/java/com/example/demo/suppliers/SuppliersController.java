66e8e6bd2cd103b9b76e9d1026757cf28e908e35
kellymo0118 <kelly230547@gmail.com>
2026-09-02 14:41:59 +0800

    feat (controller + service) 修改刪除

--- src/main/java/com/example/demo/suppliers/SuppliersController.java
+++ src/main/java/com/example/demo/suppliers/SuppliersController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,10 +24,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class SuppliersController {
     private final SuppliersService suppliersService;
 
-    //新增單筆
+    //---新增---
+    //單筆
     //驗證Email格式 AI建議放在DTO(未完待續)
     @PostMapping("/api/supplier/add")
     public Suppliers addSupplier(@RequestBody Suppliers supplier) {
+       
         Suppliers addSuppliers = suppliersService.insertSupplier(supplier.getName(), supplier.getPhone(), supplier.getAddress(), supplier.getEmail());
         
         //DTO限制回傳資料
@@ -34,7 +37,7 @@ public class SuppliersController {
         // return "新增成功";
     }
 
-    //新增多筆
+    //多筆
     @PostMapping("/api/suppliers/addAll")
     public List<Suppliers> addSuppliers(@RequestBody List<Suppliers> suppliersList) {
         //List<Suppliers> addAllSuppliers = new ArrayList<>();
@@ -43,15 +46,16 @@ public class SuppliersController {
         return suppliersService.insertSuppliers(suppliersList);
     }
 
-    //修改
+    //---修改---
     @PutMapping("/api/update/{id}")
-    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
-        //TODO: process PUT request
+    public Suppliers putMethodName(@PathVariable Long id, @RequestBody Suppliers supplier) {
+       
         
-        return entity;
+        return suppliersService.updateSupplier(id, supplier);
     }
 
-    //查詢單筆
+    //---查詢---
+    //單筆
     @GetMapping("/api/supplier/{id}")
     public Suppliers getSupplierById(@PathVariable Long id) {
         return suppliersService.findSupplierById(id)
@@ -59,14 +63,15 @@ public class SuppliersController {
                 //回傳狀態值
     }
 
-    //查詢多筆
+    //多筆
     @GetMapping("/api/supplier/list")
     public List<Suppliers> getSuppliersByIds(@RequestBody List<Long> ids) {
         List<Suppliers> suppliersList = suppliersService.findSuppliersById(ids);
         return suppliersList;
     }
 
-    //接收查詢所有供應商
+     
+    //所有供應商
     @GetMapping("/api/supplier/all")
     public List<Suppliers> AllSuppliers() {
         List<Suppliers> allSuppliers = suppliersService.listAllSuppliers();
@@ -75,8 +80,10 @@ public class SuppliersController {
         return allSuppliers;
     }
     
-    //刪除
-
-
+    //---刪除---
+    @DeleteMapping("/api/supplier/{id}")
+    public void deleteSupplier(@PathVariable Long id) {
+        suppliersService.deleteSupplier(id);
+    }
 
 }
