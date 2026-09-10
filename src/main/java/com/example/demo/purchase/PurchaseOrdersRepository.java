bf6a581d5b29a4bed95174e59941229f9b6773db
kellymo0118 <kelly230547@gmail.com>
2026-09-11 01:42:48 +0800

    delete ()

--- src/main/java/com/example/demo/purchase/PurchaseOrdersRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.purchase;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseOrdersRepository extends JpaRepository<PurchaseOrders, Long> {
-}
