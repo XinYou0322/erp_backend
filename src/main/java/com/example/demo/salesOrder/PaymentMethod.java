008aa712964429c5f0399b590fd35e1fe83a4629
kellymo0118 <kelly230547@gmail.com>
2026-09-11 01:48:47 +0800

    feat (salesOrder) entity

---
+++ src/main/java/com/example/demo/salesOrder/PaymentMethod.java
@@ -0,0 +1,7 @@
+package com.example.demo.salesOrder;
+
+public enum PaymentMethod {
+    CASH, //現金
+    CREDIT_CARD, //信用卡
+    MOBILE_PAYMENT //行動支付
+}
---
+++ src/main/java/com/example/demo/salesOrder/SalesOrderStatus.java
@@ -0,0 +1,6 @@
+package com.example.demo.salesOrder;
+
+public enum SalesOrderStatus {
+    COMPLETED, //交易已完成
+    VOIDED //交易已作廢
+}
